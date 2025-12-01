0d92cf021b80ad2886d4f100b564c0e7ee1f1023
Ayush Sinha <sinhaayush0806@.com>
2025-12-01 17:19:56 +0530

    Fixed footer text

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -114,7 +114,7 @@ const page = () => {
             </span>
             . All Rights Reserved.
           </span>
-          <Link href="https://github.com/Mahmud0808" className="me-4 md:me-6">
+          <Link href="https://github.com/ayush080603" className="me-4 md:me-6">
             <span className="hover:text-primary-500 mt-3 text-sm font-medium text-gray-500 sm:mt-0">
               Ayush Sinha and Sumit Kumar
             </span>
